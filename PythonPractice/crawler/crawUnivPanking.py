75fdc506edeb2683683505f45a4bf1e9fd6be6ea
Atlantic <756777965@qq.com>
2019-07-27 16:32:51 +0800

    完成中国大学排名显示

--- PythonPractice/crawler/crawUnivPanking.py
+++ PythonPractice/crawler/crawUnivPanking.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import bs4
 
+# 获取静态页面
 def getHTMLText(url):
     try:
         r = requests.get(url, timeout = 30)
@@ -10,12 +12,24 @@ def getHTMLText(url):
     except:
         return ''
 
+# 提取HTML页面的静态信息
 def fillUnivList(ulist, html):
-    pass
+    soup = BeautifulSoup(html, 'html.parser')
+    for tr in soup.find('tbody').children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            ulist.append([tds[0].string, tds[1].string, tds[2].string, tds[3].string])
 
+# 打印输出
 def printUnivList(ulist, num):
-    print('Suc' + str(num))
+    # 第二个花括号里面的4代表使用format的第五个参数进行填充
+    tplt = '{0:^10}\t{1:{4}^10}\t{2:^10}\t{3:^10}'
+    print(tplt.format('排名', '学校名称', '省份', '总分', chr(12288))) # 表头,多余的空格使用中文空格代替，否则会出现不对齐的现象
+    for i in range(num):
+        u = ulist[i]
+        print(tplt.format(u[0], u[1], u[2], u[3], chr(12288)))
 
+# 主函数
 def main():
     uinfo = []
     url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html'
